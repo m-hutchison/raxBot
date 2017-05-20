1592bd5da3302cb1869f338bc34a3eb860489bd6
Mathew Hutchison <mathew@hotmail.com.au>
2017-05-20 19:21:46 +1000

    Update bot.py
    Added some more functionality.
    When a new member joins the Discord, it'll welcome them and log it into a txt file.
    When they leave, it'll log it into a txt file (will make a leave message soon.

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 import asyncio
 import datetime
+import os
 
 from discord.ext import commands
 
-description = 'Custom bot'
+description = 'Custom rax bot'
 bot_prefix = '$'
 
 client = commands.Bot(description=description, command_prefix=bot_prefix)
@@ -18,14 +19,34 @@ async def on_ready():
     print(client)
 
 @client.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
+    f = open('log.txt', 'a')
+    memberjoined = str(member) + (' has joined the server @ {:%b %d %Y}\n').format(datetime.datetime.now())
+    f.write(memberjoined)
+    f.close()
+    print(memberjoined)
+	
+@client.event
+async def on_member_remove(member):
+    server = member.server
+    f = open('log.txt', 'a')
+    memberleave = str(member) + (' has left the server @ {:%b %d %Y}\n').format(datetime.datetime.now())
+    f.write(memberleave)
+    f.close()
+    print(memberleave)
+	
+@client.event
 async def on_message(message):
     if message.content.startswith('$hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('$date'):
-        msg = 'Todays date is {:%b, %d %Y} in Australia'.format(datetime.datetime.now())
-        await client.send_message(message.channel, msg)    
+        msg = 'Todays date is {:%b %d %Y} in Australia'.format(datetime.datetime.now())
+        await client.send_message(message.channel, msg)	
 
-        
-client.run('INSERT_UR_DISCORD_TOKEN_HERE_PLS')
+		
+client.run('CLIENT_TOKEN_HERE')
