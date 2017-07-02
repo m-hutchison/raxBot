6ebe0183d8bcdc5bd1f17840383e2f4541ce84fe
Mathew Hutchison <mathew@hotmail.com.au>
2017-07-02 15:55:05 +1000

    Update bot.py
    Added some logging features

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ async def on_member_join(member):
     fmt = 'Welcome {0.mention} to {1.name}!'
     await client.send_message(server, fmt.format(member, server))
     f = open('log.txt', 'a')
-    memberjoined = str(member) + (' has joined the server @ {:%b %d %Y}\n').format(datetime.datetime.now())
+    memberjoined = ('User: ') + str(member) + ('\nActn: Joined server\n') + ('Date: {:%d %b %Y}\n').format(datetime.datetime.now()) + ('-----------------------------------------------------------\n')
     f.write(memberjoined)
     f.close()
     print(memberjoined)
@@ -32,21 +32,32 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     server = member.server
+    fmt = 'The user {0.mention} has left the server!'
+    await client.send_message(server, fmt.format(member, server))
     f = open('log.txt', 'a')
-    memberleave = str(member) + (' has left the server @ {:%b %d %Y}\n').format(datetime.datetime.now())
+    memberleave = ('User: ') + str(member) + ('\nActn: Left server\n') + ('Date: {:%d %b %Y}\n').format(datetime.datetime.now()) + ('-----------------------------------------------------------\n')
     f.write(memberleave)
     f.close()
     print(memberleave)
-	
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('$date'):
-        msg = 'Todays date is {:%b %d %Y} in Australia'.format(datetime.datetime.now())
+        msg = 'The current date is {:%d %b %Y} in Australia'.format(datetime.datetime.now())
         await client.send_message(message.channel, msg)	
 
-		
-client.run('CLIENT_TOKEN_HERE')
+    if message.content.startswith('intro'):
+        msg = 'Hello {0.author.mention} I am raxbot! Nice to meet you :D'.format(message)
+        await client.send_message(message.channel, msg)	
+	
+@client.event
+async def delete_messages(messages):
+	if message.content.startswith('$clear'):
+		async for msg in client.logs_from(message.channel, limit=100):
+			await client.delete_messages(messages)		
+
+client.run('CLIENT TOKEN HERE')
